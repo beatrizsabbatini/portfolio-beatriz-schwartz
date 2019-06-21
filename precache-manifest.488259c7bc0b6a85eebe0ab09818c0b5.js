0e4ee627d9023a0e652b0d37ba149f4d533049e8
Beatriz Schwartz <beatriz.sabbatini@gmail.com>
2019-06-21 05:42:08 -0300

    Updates

---
+++ precache-manifest.488259c7bc0b6a85eebe0ab09818c0b5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bdf1b6fc1389a929a17b0db708f8e83",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/index.html"
+  },
+  {
+    "revision": "83ad457352ce3c511fb3",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/css/main.32722bc0.chunk.css"
+  },
+  {
+    "revision": "9664b040832c04ed85db",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/js/2.a2deb0b0.chunk.js"
+  },
+  {
+    "revision": "83ad457352ce3c511fb3",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/js/main.309837d8.chunk.js"
+  },
+  {
+    "revision": "1c6dba58dede30815e94",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/js/runtime~main.31e1260d.js"
+  },
+  {
+    "revision": "81c56d2fe74acfee2a1c8b5a425d737f",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/cheer.81c56d2f.jpeg"
+  },
+  {
+    "revision": "9a2460c59f6338fb9f80e2d31acc2ae2",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/cientista1.9a2460c5.gif"
+  },
+  {
+    "revision": "259cd0fade17336dd5bebba16c02e9af",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/cientista2.259cd0fa.gif"
+  },
+  {
+    "revision": "aba0738e473c33b289287f9710b72173",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/cientista3.aba0738e.gif"
+  },
+  {
+    "revision": "fdcea175d991db1ab28545a6d1a6808c",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/curriculo_2019.fdcea175.pdf"
+  },
+  {
+    "revision": "681fd83c4acc5f3e70880ab941b5af37",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/eu.681fd83c.jpg"
+  },
+  {
+    "revision": "4ffa1f20f473e8bc329065ef4872f425",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/gamejam.4ffa1f20.jpeg"
+  },
+  {
+    "revision": "b08dd46ff34600453f44da3bd3ecae02",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/geovanna.b08dd46f.png"
+  },
+  {
+    "revision": "ebf9f476b41efc5a99cf188fdaf980f2",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/github.ebf9f476.png"
+  },
+  {
+    "revision": "7da73b4c545f4886c64c6a48bf3985ee",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/lisboa.7da73b4c.jpeg"
+  },
+  {
+    "revision": "577d1c5b589058d4b2adcaa3bac07938",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/lisboa2.577d1c5b.jpg"
+  },
+  {
+    "revision": "7eda8c961e68dc9f3957a16e04773731",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/mesa.7eda8c96.jpg"
+  },
+  {
+    "revision": "3d40662dfa7369f2a35d230d01a76022",
+    "url": "/beatrizsabbatini/portfolio-beatriz-schwartz.git/static/media/zumbi.3d40662d.png"
+  }
+]);
